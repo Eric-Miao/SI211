f1c959657db42b681ba663ea7033fb1514cf67dc
ericmiao <miaoyx@shanghaitech.edu.cn>
2020-11-17 16:11:48 +0800

    clear the code for submission

--- project/adb.jl
+++ project/adb.jl
@@ -1,186 +1,286 @@
-# Golden 3 key words: Data Structure, Algorithm, User Interface
-
-# Project Requierments
-# 1. Write a generic implementation
-# 2. Write efficient code, usually time(ADforward) = 1000*time(ADbackward)
-# 3. We don't know the exact evaluation error but the results of the three various(?) should coincide up to small errors (<= 10^-6)
-
-# From a computer science perspective: (**NOT FOR REQUIREMENTS**)
-# 1. Global variables are troublesome
-# Solution: use shared pointers instead of global variables, [Maintain all variables locally, within the ADV data structure]
-# 2. Avoid redundant code:
-# => Outsource common code to a unary and binary operator. [Write generic operations / modules];
-# 3.a. Don't require the users to provide n(the order of input) ! [Write a counter instead]
-# 3.b. Return a code that returns f and f and d^Tf' [in order to ensure efficiency]
-
+# Reimplement Boris's Backward function, hope this works
 import Base.+, Base.*, Base.-, Base./, Base.sin, Base.cos
 
 NOP = 0;    # Number of operations
-FS = "";    # Forward Sweep
-BS = "";    # Backward Sweep
+FS = [];    # Forward Sweep
+BS = [];    # Backward Sweep
 
-mutable struct ADV
-    i::Int16;
-    ADV(i)=new(i)
+mutable struct ADN
+    id::Int16;
+    op;
+    a::Int16;
+    b::Int16;
+    val::Float64;
 end
 
-# ADV op. ADV
-function +(a::ADV, b::ADV)
-    i = a.i;
-    j = b.i;
-    global NOP = NOP + 1;
-    global FS = FS * "  v[$NOP] = v[$i]+v[$j]; \n";
-    global BS = "  w[$i] += w[$NOP]; \n" * BS;
-    global BS = "  w[$j] += w[$NOP]; \n" * BS;
-    return ADV(NOP);
+# mark the operators representations below
+# 0 = +
+# 1 = -
+# 2 = *
+# 3 = /
+# 4 = sin
+# 5 = cos
+
+
+# Reload
+# ADN op. ADN
+function +(a::ADN, b::ADN)
+    global NOP
+    global FS
+    global BS
+    i = a.id;
+    j = b.id;
+    NOP = NOP + 1;
+    value = a.val + b.val;
+    op = 0;
+    new=ADN(NOP,op,i,j,value)
+    push!(FS,new);
+    push!(BS,0);
+    return new;
 end
 
-function -(a::ADV, b::ADV)
-    i = a.i;
-    j = b.i;
-    global NOP = NOP + 1;
-    global FS = FS * "  v[$NOP] = v[$i]-v[$j]; \n";
-    global BS = "  w[$i] += w[$NOP]; \n" * BS;
-    global BS = "  w[$j] += -w[$NOP]; \n" * BS;
-    return ADV(NOP);
+function -(a::ADN, b::ADN)
+    global NOP
+    global FS
+    global BS
+    i = a.id;
+    j = b.id;
+    NOP = NOP + 1;
+    value = a.val - b.val;
+    op = 1;
+    new=ADN(NOP,op,i,j,value)
+    push!(FS,new);
+    push!(BS,0);
+    return new;
 end
 
-function /(a::ADV, b::ADV)
-    i = a.i;
-    j = b.i;
-    global NOP = NOP + 1;
-    global FS = FS * "  v[$NOP] = v[$i]/v[$j]; \n";
-    global BS = "  w[$i] += 1/v[$j]*w[$NOP]; \n" * BS;
-    global BS = "  w[$j] += -1*v[$i]/(v[$j]^2)*w[$NOP]; \n" * BS;
-    return ADV(NOP);
+function *(a::ADN, b::ADN)
+    global NOP
+    global FS
+    global BS
+    i = a.id;
+    j = b.id;
+    NOP = NOP + 1;
+    value = a.val + b.val;
+    op = 2;
+    new=ADN(NOP,op,i,j,value)
+    push!(FS,new);
+    push!(BS,0);
+    return new;
 end
 
-function *(a::ADV, b::ADV)
-    i = a.i;
-    j = b.i;
-    global NOP = NOP + 1;
-    global FS = FS * "  v[$NOP] = v[$i]*v[$j]; \n";
-    global BS = "  w[$i] += v[$j]*w[$NOP]; \n" * BS;
-    global BS = "  w[$j] += v[$i]*w[$NOP]; \n" * BS;
-    return ADV(NOP);
-end
-
-function sin(a::ADV)
-    i = a.i;
-    global NOP = NOP + 1;
-    global FS = FS * "  v[$NOP] = sin(v[$i]); \n";
-    global BS = "  w[$i] += cos(v[$i]) * w[$NOP]; \n" * BS;
-    return ADV(NOP);
-end
-
-function cos(a::ADV)
-    i=a.i
-    global NOP=NOP + 1
-    global FS = FS * "  v[$NOP] = cos(v[$i]); \n";
-    global BS = "  w[$i] += -sin(v[$i]) * w[$NOP]; \n" * BS;
-    return ADV(NOP);
+function /(a::ADN, b::ADN)
+    global NOP
+    global FS
+    global BS
+    i = a.id;
+    j = b.id;
+    NOP = NOP + 1;
+    value = a.val / b.val;
+    op = 3;
+    new=ADN(NOP,op,i,j,value)
+    push!(FS,new);
+    push!(BS,0);
+    return new;
 end
 
 
-# Float64 op. ADV
-function +(a::Float64, b::ADV)
-    j = b.i;
-    global NOP = NOP + 1;
-    global FS = FS * "  v[$NOP] = $a+v[$j]; \n";
-    global BS = "  w[$j] += w[$NOP]; \n" * BS;
-    return ADV(NOP);
+function sin(a::ADN)
+    global NOP
+    global FS
+    global BS
+    i = a.id;
+    j = -1
+    NOP = NOP + 1;
+    value = sin(a.val);
+    op = 4;
+    new=ADN(NOP,op,i,j,value)
+    push!(FS,new);
+    push!(BS,0);
+    return new;
 end
 
-function +(a::ADV, b::Float64)
+
+function cos(a::ADN)
+    global NOP
+    global FS
+    global BS
+    i = a.id;
+    j = -1;
+    NOP = NOP + 1;
+    value = cos(a.val);
+    op = 5;
+    new=ADN(NOP,op,i,j,value)
+    push!(FS,new);
+    push!(BS,0);
+    return new;
+end
+
+
+# Float64 op. ADN
+function +(a::Float64, b::ADN)
+    global NOP
+    global FS
+    global BS
+    i = b.id;
+    j = -1
+    NOP = NOP + 1;
+    value = a+b.val;
+    op = 0;
+    new=ADN(NOP,op,i,j,value)
+    push!(FS,new);
+    push!(BS,0);
+    return new;
+end
+
+function +(a::ADN, b::Float64)
     return b+a;
 end
 
-function -(a::Float64, b::ADV)
+function -(a::Float64, b::ADN)
     return (-a)+b;
 end
 
-function -(a::ADV, b::Float64)
+function -(a::ADN, b::Float64)
     return a+(-b);
 end
 
-function /(a::Float64, b::ADV)
-    j = b.i;
-    global NOP = NOP + 1;
-    global FS = FS * "  v[$NOP] = $a/v[$j]; \n";
-    global BS = "  w[$j] += -1*$a/(v[$j]*v[$j])*w[$NOP]; \n" * BS;
-    return ADV(NOP)
+function /(a::Float64, b::ADN)
+    global NOP
+    global FS
+    global BS
+    j = b.id;
+    i = -1
+    NOP = NOP + 1;
+    value = a/b.val;
+    op = 3;
+    new=ADN(NOP,op,i,j,value)
+    push!(FS,new);
+    push!(BS,0);
+    return new;
 end
 
-function /(a::ADV, b::Float64)
-    i=a.i;
-    global NOP = NOP + 1;
-    global FS = FS * "  v[$NOP] = v[$i]/$b; \n";
-    global BS = "  w[$i] += w[$NOP]/$b; \n" * BS
-    return ADV(NOP)
+function /(a::ADN, b::Float64)
+    global NOP
+    global FS
+    global BS
+    i = a.id;
+    j = -1
+    NOP = NOP + 1;
+    value = a.val/b;
+    op = 3;
+    new=ADN(NOP,op,i,j,value)
+    push!(FS,new);
+    push!(BS,0);
+    return new;
 end
 
-function *(a::Float64, b::ADV)
-    j = b.i;
-    global NOP = NOP + 1;
-    global FS = FS * "  v[$NOP] = $a*v[$j]; \n";
-    global BS = "  w[$j] += $a*w[$NOP]; \n" * BS;
-    return ADV(NOP)
+function *(a::Float64, b::ADN)
+    global NOP
+    global FS
+    global BS
+    i = b.id;
+    j = -1
+    NOP = NOP + 1;
+    value = a*b.val;
+    op = 2;
+    new=ADN(NOP,op,i,j,value)
+    push!(FS,new);
+    push!(BS,0);
+    return new;
 end
 
-function *(a::ADV, b::Float64)
+function *(a::ADN, b::Float64)
     return b*a;
 end
 
+function ADbackward(f)
+    function g(x,d)
+        global FS=[]
+        global BS=[]
+        global NOP=0
+        xx=[]
+        n = length(x);
+        j = -1;
+        i = -1;
+        op = -1;
+        #println(BS)
+        #println(FS)
+        for i = 1:n
+            NOP = NOP+1;
+            new = ADN(NOP,op,i,j,x[i])
+            push!(FS,new);
+            push!(BS,0)
+            push!(xx,new)
+        end
+        y=f(xx)
+        #println(y)
+        m=length(y)
+        #println(m)
+        #println(FS)
+        #println(BS)
+        for i=1:m
+            #println(y[i].id)
+            BS[y[i].id]=d[i]
+        end
+        #println(BS)
+        #println(NOP)
+        for j = 1:NOP
+            i = NOP+1-j
+            temp=FS[i]
+            op=temp.op
+            #print(op)
+            # +
+            if op == 0
+                #println(BS[temp.a],BS[temp.id])
+                BS[temp.a]+=BS[temp.id]
+                if temp.b != -1
+                    BS[temp.b]+=BS[temp.id]
+                end
+            # -
+            elseif op == 1
+                BS[temp.a]+=BS[temp.id]
+                BS[temp.b]+=(-1)*BS[temp.id]
+            # *
+            elseif op == 2
+                if temp.b != -1
+                    BS[temp.a]+=FS[temp.b].val*BS[temp.id]
+                    BS[temp.b]+=FS[temp.a].val*BS[temp.id] 
+                else 
+                    b=FS[temp.id].val/FS[temp.a].val
+                    BS[temp.a]+=b*BS[temp.id]
+                end
+            # /
+            elseif op == 3
+                if temp.a != -1 & temp.b != -1
+                    BS[temp.a]+=(1/FS[temp.b].val)*BS[temp.id]
+                    BS[temp.b]+=(-1)*(FS[temp.a].val/FS[temp.b].val^2)*BS[temp.id]
+                elseif temp.a != -1
+                    b = FS[temp.a].val/FS[temp.id].val
+                    BS[temp.a]+=(1/b)*BS[temp.id]
+                elseif temp.b != -1
+                    a = FS[temp.b].val*FS[temp.id].val
+                    BS[temp.b]+=(-1)*(a/FS[temp.b].val^2)*BS[temp.id]
+                end
+            # sin
+            elseif op == 4
+                BS[temp.a]+=cos(FS[temp.a].val)*BS[temp.id]
+            
+            # cos
+            elseif op ==5
+                BS[temp.a]+=-sin(FS[temp.a].val)*BS[temp.id]
 
-
-function ADbackward(f, n)
-    global FS = "";
-    global BS = "";
-    
-    xx = [];
-    for i=1:n
-        global FS = FS*"  v[$i] = x[$i]; \n";
-        xx = [xx; ADV(i)];
+            end
+        end
+        #println(FS)
+        #println(BS)
+        return BS[1:n]
     end
-
-    global NOP = n;
-
-    yy = f(xx);
-    m = length(yy);
-    program = "function h(x, d) \n";
-    program *= "  v = zeros($NOP); \n";
-    program *= "  w = zeros($NOP); \n";
-    program *= "  y = zeros($m); \n";
-    program *= "  ret = zeros($n); \n";
-    program *= FS;
-
-    for i=1:m
-        j = yy[i].i;
-        program *= "  w[$j] = d[$i]; \n";
-    end
-
-    program *= BS;
-    #for i=1:m
-    #    program *= "  y[$i] = w[$i]; \n";
-    #end
-
-    for i=1:n
-        program *= "  ret[$i] = w[$i]; \n"
-    end
-    program *= "  return ret; \n";
-    program *= "end \n";
-
-    # Uncomment this to check
-    # println(program);
-    # Transform the string into executable code && Execute it 
-    eval( Meta.parse(program) );
-    return h;
+    return g;
 end
 
-function f(x)
-    return [x[1] * x[2]+2.0; 5.0*x[1] / x[2]*x[2]/2.0;sin(x[1]);cos(x[2])*x[1]];
-end
-
-# main()
-h = ADbackward(f,2);
-print(h([1,1], [1,1,1,1])); #[2, 3]
+#function f(x)
+#    return [x[1] * x[2]+2.0; 5.0*x[1] / x[2]*x[2]/2.0;sin(x[1]);cos(x[2])*x[1]];
+#end
+#g = ADbackward(f);
+#print(g([1,1], [1,1,1,1]));
